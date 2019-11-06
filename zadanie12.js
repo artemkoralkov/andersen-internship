9b1aad6a65eeaa3a8c3aadcc19f4b2ed03985c9d
artemkoralkov <artemkoralkov@gmail.com>
2019-11-06 11:38:34 +0300

    Added tasks to par1

--- zadanie12.js
+++
@@ -1,8 +0,0 @@
-/* Вводится четырёхзначное число (abcd). Вывести сумму ab + cd. */
-let nmb = 1234;
-nmb = String(nmb);
-let ab = nmb.slice(0, 2);
-ab = +ab;
-let cd = nmb.slice(2);
-cd = +cd;
-console.log(ab + cd);

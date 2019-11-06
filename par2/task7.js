5bdcceb4c2047d7f26f81b5056a7fc15ad69706f
artemkoralkov <artemkoralkov@gmail.com>
2019-11-06 11:48:53 +0300

    Correct with hours, minutes, seconds, milliseconds

--- par2/task7.js
+++ par2/task7.js
@@ -2,7 +2,11 @@ const currentDate = new Date();
 const nextDate = new Date(
   currentDate.getFullYear(),
   currentDate.getMonth(),
-  currentDate.getDate() + 100
+  currentDate.getDate() + 100,
+  currentDate.getHours(),
+  currentDate.getMinutes(),
+  currentDate.getSeconds(),
+  currentDate.getUTCMilliseconds()
 );
 console.log(currentDate);
 console.log(nextDate);

619798d0f416ae82ddf9c305b3ed62bfdacb2f71
artemkoralkov <artemkoralkov@gmail.com>
2019-11-09 15:56:16 +0300

    Добавил условия

--- par2/task7.js
+++ par2/task7.js
@@ -1,12 +1,9 @@
+/*  С помощью встроенного объекта Date 
+получить текущую дату и вычислить дату, которая
+наступит через 100 дней.
+*/
 const currentDate = new Date();
-const nextDate = new Date(
-  currentDate.getFullYear(),
-  currentDate.getMonth(),
-  currentDate.getDate() + 100,
-  currentDate.getHours(),
-  currentDate.getMinutes(),
-  currentDate.getSeconds(),
-  currentDate.getMilliseconds()
-);
+const nextDate = new Date();
+nextDate.setDate(currentDate.getDate() + 100);
 console.log(currentDate);
 console.log(nextDate);

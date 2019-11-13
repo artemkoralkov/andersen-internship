74e54ac6382db5a1b7ccaf005bf8e5ef5ecf60eb
artemkoralkov <artemkoralkov@gmail.com>
2019-11-13 13:06:07 +0300

    Исправил 9 задание

--- par3/task9.js
+++ par3/task9.js
@@ -5,8 +5,16 @@ var arr = [1, 2, 3, 4, 5, 6, 7];
 console.log( filter(arr, function(a) { return a % 2 == 0 }) ); // 2,4,6
 */
 function filter(arr, func) {
-  return arr.filter(func);
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const a = arr[i];
+    if (func(a)) {
+      newArr.push(a);
+    }
+  }
+  return newArr;
 }
+
 const arr = [1, 2, 3, 4, 5, 6, 7];
 console.log(
   filter(arr, function(a) {
